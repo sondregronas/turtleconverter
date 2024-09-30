f729a0054f967b54b764fea12299c0a56ec11875
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-09-30 15:55:06 +0200

    fix: remove print statement

--- turtleconverter/__init__.py
+++ turtleconverter/__init__.py
@@ -25,7 +25,6 @@ def generate_static_files(static_folder: Path = Path('static'), assets_folder: P
 newline_blockers = [r'```', r'\|']
 except_on = ['\n', r'\|']
 newline_regex = re.compile(rf'({"|".join(newline_blockers)})\n(?!{"|".join(except_on)})')
-print(newline_regex)
 
 
 def ensure_nl2br(md_file_path: Path) -> Path:
