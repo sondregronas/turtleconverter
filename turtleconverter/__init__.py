731820306ef88ab3683752d77b982694df099f97
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-10-24 10:59:39 +0200

    format: ruff

--- turtleconverter/__init__.py
+++ turtleconverter/__init__.py
@@ -12,55 +12,85 @@ except ImportError:
 
 def _str_to_path_mass_convert(list_of_values: list) -> list:
     """Converts a list of strings to a list of Path objects."""
-    return [Path(value) if isinstance(value, str) else value for value in list_of_values]
+    return [
+        Path(value) if isinstance(value, str) else value for value in list_of_values
+    ]
 
 
-def generate_static_files(static_folder: Path = Path('static'), assets_folder: Path = Path('turtleconvert')) -> None:
+def generate_static_files(
+    static_folder: Path = Path("static"), assets_folder: Path = Path("turtleconvert")
+) -> None:
     """Generates the static files for the HTML file."""
-    static_folder, assets_folder = _str_to_path_mass_convert([static_folder, assets_folder])
+    static_folder, assets_folder = _str_to_path_mass_convert(
+        [static_folder, assets_folder]
+    )
     _build(None, static_folder / assets_folder, MKDOCS_CONFIG, only_static_files=True)
 
 
 # If we only have a single newline after ``` or |, add a second \n
-newline_blockers = [r'```', r'\|']
-except_on = ['\n', r'\|']
-newline_regex = re.compile(rf'({"|".join(newline_blockers)})\n(?!{"|".join(except_on)})')
+newline_blockers = [r"```", r"\|"]
+except_on = ["\n", r"\|"]
+newline_regex = re.compile(
+    rf'({"|".join(newline_blockers)})\n(?!{"|".join(except_on)})'
+)
 
 
 def ensure_nl2br(md_file_path: Path) -> Path:
     """Ensures that all newlines in a markdown file are converted to <br> tags."""
     # Create a temporary file in memory to store the new content
-    with open(md_file_path, 'r', encoding='utf-8') as f:
+    with open(md_file_path, "r", encoding="utf-8") as f:
         content = f.read()
-    with tempfile.NamedTemporaryFile('w', delete=False, encoding='utf-8') as f:
-        f.write(newline_regex.sub(r'\1\n\n', content))
+    with tempfile.NamedTemporaryFile("w", delete=False, encoding="utf-8") as f:
+        f.write(newline_regex.sub(r"\1\n\n", content))
     return Path(f.name)
 
 
-def mdfile_to_html(md_file_path: Path, static_folder: Path = Path('static'),
-                   assets_folder: Path = Path('turtleconvert'), include_metadata: bool = False,
-                   abspath: bool = True, template: Path = 'turtleconvert.html',
-                   generate_static_files: bool = False) -> str or tuple:
+def mdfile_to_html(
+    md_file_path: Path,
+    static_folder: Path = Path("static"),
+    assets_folder: Path = Path("turtleconvert"),
+    include_metadata: bool = False,
+    abspath: bool = True,
+    template: Path = "turtleconvert.html",
+    generate_static_files: bool = False,
+) -> str or tuple:
     """Converts a markdown file to a html file."""
-    md_file_path, static_folder, assets_folder = _str_to_path_mass_convert([md_file_path, static_folder, assets_folder])
+    md_file_path, static_folder, assets_folder = _str_to_path_mass_convert(
+        [md_file_path, static_folder, assets_folder]
+    )
     # Create a temporary file to store the new content so we can ensure we have correct newlines
     temp_file = ensure_nl2br(md_file_path)
-    page, meta = _build(temp_file, static_folder / assets_folder, MKDOCS_CONFIG, template=template,
-                        generate_static_files=generate_static_files)
+    page, meta = _build(
+        temp_file,
+        static_folder / assets_folder,
+        MKDOCS_CONFIG,
+        template=template,
+        generate_static_files=generate_static_files,
+    )
     temp_file.unlink()
 
-    page = page.replace('../assets/', {True: f'/{static_folder}/{assets_folder}/',
-                                       False: f'../{static_folder}/{assets_folder}/'}[abspath])
+    page = page.replace(
+        "../assets/",
+        {
+            True: f"/{static_folder}/{assets_folder}/",
+            False: f"../{static_folder}/{assets_folder}/",
+        }[abspath],
+    )
 
     if include_metadata:
         return page, meta
     return page
 
 
-def mdfile_to_sections(md_file_path: Path, static_folder: Path = Path('static'),
-                       assets_folder: Path = Path('turtleconvert'), remove_heading: bool = True,
-                       abspath: bool = True, template: Path = 'turtleconvert.html',
-                       generate_static_files: bool = False) -> dict:
+def mdfile_to_sections(
+    md_file_path: Path,
+    static_folder: Path = Path("static"),
+    assets_folder: Path = Path("turtleconvert"),
+    remove_heading: bool = True,
+    abspath: bool = True,
+    template: Path = "turtleconvert.html",
+    generate_static_files: bool = False,
+) -> dict:
     """Returns a dictionary of HTML content divided into sections.
     {
         "heading": "My Markdown File!",
@@ -69,49 +99,61 @@ def mdfile_to_sections(md_file_path: Path, static_folder: Path = Path('static'),
         "meta": { <All the metadata (frontmatter) from the markdown file as a dictionary> }
     }
     """
-    md_file_path, static_folder, assets_folder = _str_to_path_mass_convert([md_file_path, static_folder, assets_folder])
+    md_file_path, static_folder, assets_folder = _str_to_path_mass_convert(
+        [md_file_path, static_folder, assets_folder]
+    )
 
-    page, meta = mdfile_to_html(md_file_path, static_folder, assets_folder, include_metadata=True, abspath=abspath,
-                                template=template, generate_static_files=generate_static_files)
+    page, meta = mdfile_to_html(
+        md_file_path,
+        static_folder,
+        assets_folder,
+        include_metadata=True,
+        abspath=abspath,
+        template=template,
+        generate_static_files=generate_static_files,
+    )
 
-    head_and_body = re.findall(r"<head.*?>(.*?)</head>.*?<body.*?>(.*?)</body>", page, re.DOTALL)
+    head_and_body = re.findall(
+        r"<head.*?>(.*?)</head>.*?<body.*?>(.*?)</body>", page, re.DOTALL
+    )
 
     head = head_and_body[0][0]
     body = head_and_body[0][1]
-    h1_tag = meta.get('title', re.search(r'<h1.*?\>(.+?)\<\/h1>', body).group(1))
+    h1_tag = meta.get("title", re.search(r"<h1.*?\>(.+?)\<\/h1>", body).group(1))
 
     if remove_heading:
-        body = re.sub(r'<h1.*?\>(.+?)\<\/h1>', '', body, count=1)
+        body = re.sub(r"<h1.*?\>(.+?)\<\/h1>", "", body, count=1)
 
-    return {
-        'heading': h1_tag,
-        'head': head,
-        'body': body,
-        'meta': meta
-    }
+    return {"heading": h1_tag, "head": head, "body": body, "meta": meta}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Testing data, feel free to ignore this
     import timeit
 
     generate_static_files()
-    print(mdfile_to_sections('test.md', template='../example_override.html'))
+    print(mdfile_to_sections("test.md", template="../example_override.html"))
 
     start_time = timeit.default_timer()
     for _ in range(100):
-        data = mdfile_to_html(Path('test.md'), abspath=False, template='../example_override.html')
+        data = mdfile_to_html(
+            Path("test.md"), abspath=False, template="../example_override.html"
+        )
     end_time = timeit.default_timer()
     delta = end_time - start_time
-    print(f'{delta:.10f} x100')
+    print(f"{delta:.10f} x100")
 
     start_time = timeit.default_timer()
-    data = mdfile_to_html(Path('test.md'), abspath=False, template='../example_override.html')
+    data = mdfile_to_html(
+        Path("test.md"), abspath=False, template="../example_override.html"
+    )
     end_time = timeit.default_timer()
-    print(f'{end_time - start_time:.10f} x1')
-    data = data.replace('Automatically generated',
-                        f'Automatically generated (in {end_time - start_time:.3f} seconds, excluding static files, 100 iterations took {delta:.3f} seconds)')
-    with open('static/test.html', 'w', encoding='utf-8') as f:
+    print(f"{end_time - start_time:.10f} x1")
+    data = data.replace(
+        "Automatically generated",
+        f"Automatically generated (in {end_time - start_time:.3f} seconds, excluding static files, 100 iterations took {delta:.3f} seconds)",
+    )
+    with open("static/test.html", "w", encoding="utf-8") as f:
         f.write(data)
 
     # Import partial to create a custom function for sections using separate paths
--- turtleconverter/mkdocs_build_override.py
+++ turtleconverter/mkdocs_build_override.py
@@ -4,19 +4,18 @@ This is a messy hack to override the mkdocs build command to convert a single ma
 
 from __future__ import annotations
 
-from pathlib import Path
-
 import mkdocs.config
 from mkdocs.commands.build import *
 from mkdocs.commands.build import _build_page, _populate_page
+from pathlib import Path
 
-if not os.path.exists(Path(__file__).parent / 'docs'):
-    os.makedirs(Path(__file__).parent / 'docs')
-MKDOCS_CONFIG = mkdocs.config.load_config(str(Path(__file__).parent / 'mkdocs.yml'))
+if not os.path.exists(Path(__file__).parent / "docs"):
+    os.makedirs(Path(__file__).parent / "docs")
+MKDOCS_CONFIG = mkdocs.config.load_config(str(Path(__file__).parent / "mkdocs.yml"))
 
 # Document variables, these normally get set in the build command.
 # Storing them here allows us to reuse the build command without having to set them again. (which is slow)
-__logger = logging.getLogger('mkdocs')
+__logger = logging.getLogger("mkdocs")
 __logger.setLevel(logging.ERROR)
 __inclusion = InclusionLevel.is_included
 __config = MKDOCS_CONFIG.plugins.on_config(MKDOCS_CONFIG)
@@ -40,18 +39,18 @@ class ConversionError(Exception):
 
 
 def _build_page(
-        page: Page,
-        config: MkDocsConfig,
-        doc_files: Sequence[File],
-        nav: Navigation,
-        env: jinja2.Environment,
-        template: str = 'turtleconvert.html',
+    page: Page,
+    config: MkDocsConfig,
+    doc_files: Sequence[File],
+    nav: Navigation,
+    env: jinja2.Environment,
+    template: str = "turtleconvert.html",
 ) -> tuple[str, any]:
     """Pass a Page to theme template and write output to site_dir."""
     try:
         context = get_context(nav, doc_files, config, page)
 
-        if template != 'turtleconvert.html':
+        if template != "turtleconvert.html":
             if not isinstance(template, Path):
                 template = Path(template)
             # The path must be absolute from the cwd.
@@ -59,15 +58,19 @@ def _build_page(
 
             # Write a file to the overrides folder called "custom_template.html" with the contents of the custom template
             # this is required because the template must be in the same directory as the mkdocs.yml file :(
-            with open(Path(__file__).parent / 'overrides' / 'custom_template.html', 'w+') as f:
+            with open(
+                Path(__file__).parent / "overrides" / "custom_template.html", "w+"
+            ) as f:
                 f.write(template_path.read_text())
 
-            template = env.get_template('custom_template.html')
+            template = env.get_template("custom_template.html")
         else:
-            template = env.get_template('turtleconvert.html')
+            template = env.get_template("turtleconvert.html")
 
         # Run `page_context` plugin events.
-        context = config.plugins.on_page_context(context, page=page, config=config, nav=nav)
+        context = config.plugins.on_page_context(
+            context, page=page, config=config, nav=nav
+        )
 
         # Render the template.
         output = template.render(context)
@@ -78,32 +81,41 @@ def _build_page(
         return output, page.meta
 
     except Exception as e:
-        raise ConversionError(f'{e}')
+        raise ConversionError(f"{e}")
 
 
-def _build(fp: Path, static_folder: Path = 'static', config: MkDocsConfig = MKDOCS_CONFIG, *,
-           template: str = 'turtleconvert.html', only_static_files: bool = False,
-           generate_static_files: bool = False) -> tuple[str, dict] or None:
+def _build(
+    fp: Path,
+    static_folder: Path = "static",
+    config: MkDocsConfig = MKDOCS_CONFIG,
+    *,
+    template: str = "turtleconvert.html",
+    only_static_files: bool = False,
+    generate_static_files: bool = False,
+) -> tuple[str, dict] or None:
     try:
         if generate_static_files or only_static_files:
             for file in __files:
                 file.dest_dir = static_folder.resolve()
-                if file.src_uri == 'assets/images/favicon.png':
+                if file.src_uri == "assets/images/favicon.png":
                     file.inclusion = InclusionLevel.EXCLUDED
-                file.dest_uri = file.dest_uri.replace('assets/', '')
+                file.dest_uri = file.dest_uri.replace("assets/", "")
             __files.copy_static_files(dirty=False, inclusion=__inclusion)
         if only_static_files:
             return
 
-        file = File(fp.name, src_dir=str(fp.parent.resolve()), dest_dir=config.site_dir,
-                    use_directory_urls=config.use_directory_urls, dest_uri=f"{fp.stem}/index.html",
-                    inclusion=InclusionLevel.INCLUDED)
+        file = File(
+            fp.name,
+            src_dir=str(fp.parent.resolve()),
+            dest_dir=config.site_dir,
+            use_directory_urls=config.use_directory_urls,
+            dest_uri=f"{fp.stem}/index.html",
+            inclusion=InclusionLevel.INCLUDED,
+        )
         file.page = Page(None, file, config)
         _populate_page(file.page, config, __files)
 
-        return _build_page(
-            file.page, config, [file], __nav, __env, template=template
-        )
+        return _build_page(file.page, config, [file], __nav, __env, template=template)
 
     except Exception as e:
         raise ConversionError(f"Error building page '{fp}': {e}")
